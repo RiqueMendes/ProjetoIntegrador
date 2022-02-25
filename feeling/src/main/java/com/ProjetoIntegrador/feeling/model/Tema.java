cb1f503bb9c6949af25231ff7347bb57e5778cab
Paulo <analu.lamas@yahoo.com.br>
2022-02-25 09:51:45 -0300

    @nalulamas

---
+++ feeling/src/main/java/com/ProjetoIntegrador/feeling/model/Tema.java
@@ -0,0 +1,51 @@
+package com.ProjetoIntegrador.feeling.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_tema")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull
+	@Size(min = 1, max = 200)
+	private String topic;
+	
+	@NotNull
+	@Size(min=5, max=1000)
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}

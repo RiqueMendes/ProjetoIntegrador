1f66779d00e48fb93ba75d24529e2ab71aba1147
malufilinto <marialuiza.filinto@gmail.com>
2022-03-03 16:20:03 -0300

    @malufilinto [ add theme relationship]

--- src/main/java/com/ProjetoIntegrador/feeling/model/Theme.java
+++ src/main/java/com/ProjetoIntegrador/feeling/model/Theme.java
@@ -1,5 +1,9 @@
 package com.ProjetoIntegrador.feeling.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+
 /*
     Responsavel pela criação da tabela Theme, com os parametros determinados pelo comando "size",
     e também foram adicionados os getters e setters.
@@ -9,9 +13,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_theme")
@@ -28,6 +35,10 @@ public class Theme {
 	@NotNull
 	@Size(min=5, max=1000)
 	private String description;
+
+	@OneToMany(mappedBy = "theme", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("theme")
+	private List <Post> post;
 
 	public long getId() {
 		return id;

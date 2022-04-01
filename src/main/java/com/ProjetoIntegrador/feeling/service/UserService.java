0fe34c2d617a26aa561af802f89b7917114d9184
Vitor Melo <vitormelo9913@gmail.com>
2022-04-01 10:51:21 -0300

    @vitor-pm [Task: User list/delete/get/update]

--- src/main/java/com/ProjetoIntegrador/feeling/service/UserService.java
+++ src/main/java/com/ProjetoIntegrador/feeling/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ProjetoIntegrador.feeling.service;
 
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.Optional;
 
 import com.ProjetoIntegrador.feeling.model.UserLoginDTO;
@@ -10,6 +11,7 @@ import com.ProjetoIntegrador.feeling.repository.UserRepository;
 import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -58,6 +60,51 @@ public class UserService {
             return user;
         }
         return null;
+    }
+
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity deleteById(Long id) {
+        Optional<UserModel> user = repository.findById(id);
+
+        if (user.isPresent()) {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<List<UserModel>> listUsers(){
+        List<UserModel> users = repository.findAll();
+        
+        if (users.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(users);
+        }
+    }
+
+    public ResponseEntity<UserModel> findById(Long id) {
+        Optional<UserModel> user = repository.findById(id);
+
+        if (user.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(user.get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado");
+        }
+    }
+
+    public ResponseEntity<UserModel> updateUser(UserModel user) {
+        Optional<UserModel> userM = repository.findById(user.getId());
+
+        if (userM.isPresent()) {
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            user.setPassword(encoder.encode(user.getPassword()));
+            repository.save(user);
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuário não encontrado");
+        }
     }
 
 }
